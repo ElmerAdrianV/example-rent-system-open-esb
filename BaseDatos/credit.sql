b9d2ab72047c60fe41fc2afdc77f1c5e90a9bdd6
Elmer Adrian Ortega Valdes <71801888+ElmerAdrianV@users.noreply.github.com>
2024-12-03 10:36:46 -0600

    Update credit.sql

--- BaseDatos/credit.sql
+++ BaseDatos/credit.sql
@@ -1,15 +1,26 @@
--- Crear tabla `credito` para referencias cruzadas de financiamiento
-CREATE TABLE credit (
-    credito_id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,
-    customer_id INT NOT NULL, -- Relación con customers
-    credit_amount DECIMAL(10, 2) NOT NULL, -- Cantidad de crédito asignada
-    credit_used DECIMAL(10, 2) DEFAULT 0,  -- Cantidad de crédito utilizada
-    CONSTRAINT fk_credit_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
+-- Conexión a la base de datos
+CONNECT 'jdbc:derby://localhost:1527/bd_proyecto_credit;user=app;password=app';
 
--- Insertar datos en `credito`
-INSERT INTO credit (customer_id, credit_amount, credit_used)
-VALUES
-(1, 5000.00, 0.00),
-(2, 10000.00, 2000.00),
-(3, 7000.00, 1000.00);
+-- Eliminar tabla existente si es necesario
+DROP TABLE credit;
+
+CREATE  TABLE  credit
+(
+  id INT  NOT NULL GENERATED ALWAYS AS IDENTITY 
+     (START WITH 1 ,INCREMENT BY 1) 
+     CONSTRAINT CUSTOMER_PK PRIMARY KEY,
+  name        VARCHAR(45) NOT NULL ,
+  email       VARCHAR(45) NOT NULL ,
+  phone       VARCHAR(45) NOT NULL ,
+  address     VARCHAR(45) NOT NULL ,
+  city_region VARCHAR(2) NOT NULL ,
+  cc_number   VARCHAR(19) NOT NULL,
+  credito     DECIMAL(10,2) NOT NULL ,  
+  );
+
+INSERT INTO credit (name,email,phone,address,city_region,cc_number,credito) VALUES
+('Ditirambo Farfulla','ditirambo.farfulla@mkt.bond','56284000','Arroyo Bajo 10 int 10','AO','123456',1000.0),
+('Gandulfo Roncante','gandulfo.roncante@mkt.bond','56284000','Arroyo Bajo 10 int 12','AO','123456',1000.0),
+('Vagonzo Durmiente','vagonzo.durminte@mkt.bond','56284000','Arroyo Bajo 10 int 45','AO','123456',1000.0),
+('Hambrosio Comensal','hambrosio.comensal@mkt.bond','56284000','Arroyo Bajo 10 int 58','AO','123456',1000.0);
+
