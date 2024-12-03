cc9696d3f6cf73309ca2f220ce95628b67c49767
Elmer Adrian Ortega Valdes <71801888+ElmerAdrianV@users.noreply.github.com>
2024-12-03 10:45:41 -0600

    Update reservations.sql

--- BaseDatos/reservations.sql
+++ BaseDatos/reservations.sql
@@ -1,3 +1,9 @@
+-- Conexión a la base de datos
+CONNECT 'jdbc:derby://localhost:1527/db_project_reservations;user=app;password=app';
+
+-- Eliminar tabla existente si es necesario
+DROP TABLE reservations;
+
 CREATE TABLE reservations (
     reservation_id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,
     customer_id INT NOT NULL,                -- Identificador del cliente (relación con customers)
@@ -15,3 +21,5 @@ VALUES
 (1, 1, '2024-12-01', '2024-12-10', 'El cliente ha solicitado un Toyota Corolla para un viaje de negocios. Todo en regla.'),
 (2, 2, '2024-11-25', '2024-12-02', 'Renta de una Ford F-150 para mudanza. Cliente pidió extensión del seguro.'),
 (3, 3, '2024-11-30', '2024-12-05', 'Se rentó un Tesla Model S. Sin reportes adicionales.');
+-- Desconectar
+DISCONNECT;
