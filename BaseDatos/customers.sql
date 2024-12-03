f2a3f192c04e004fba2d7a53962011665c9b1113
Elmer Adrian Ortega Valdes <71801888+ElmerAdrianV@users.noreply.github.com>
2024-12-03 10:37:51 -0600

    Update customers.sql

--- BaseDatos/customers.sql
+++ BaseDatos/customers.sql
@@ -1,3 +1,9 @@
+-- Conexión a la base de datos
+CONNECT 'jdbc:derby://localhost:1527/bd_proyecto_customers;user=app;password=app';
+
+-- Eliminar tabla existente si es necesario
+DROP TABLE customers;
+
 CREATE TABLE customers (
     customer_id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,         -- Nombre del cliente
@@ -15,3 +21,5 @@ VALUES
 ('Juan', 'Pérez', 'Mexicana', '1990-05-15', 'JPMX123456', 'Calle Falsa 123, CDMX, México', '12345678901234567890'),
 ('Maria', 'Gómez', 'Estadounidense', '1985-03-22', 'MGUSA654321', '456 Elm Street, Los Angeles, USA', '09876543210987654321'),
 ('Carlos', 'López', 'Canadiense', '1992-11-10', 'CLCAN987654', '789 Maple Ave, Toronto, Canada', '11122233344455566677');
+-- Desconectar
+DISCONNECT;
