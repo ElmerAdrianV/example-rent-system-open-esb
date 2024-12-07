ed88ea8e4f2d55c708f79218f10e607fb6bdbd90
Elmer Adrian Ortega Valdes <71801888+ElmerAdrianV@users.noreply.github.com>
2024-12-07 14:25:54 -0600

    Update credit.sql

--- BaseDatos/credit.sql
+++ BaseDatos/credit.sql
@@ -22,7 +22,10 @@ INSERT INTO credit (name,email,phone,address,city_region,cc_number,credito) VALU
 ('Ditirambo Farfulla','ditirambo.farfulla@mkt.bond','56284000','Arroyo Bajo 10 int 10','AO','123456',1000.0),
 ('Gandulfo Roncante','gandulfo.roncante@mkt.bond','56284000','Arroyo Bajo 10 int 12','AO','123456',1000.0),
 ('Vagonzo Durmiente','vagonzo.durminte@mkt.bond','56284000','Arroyo Bajo 10 int 45','AO','123456',1000.0),
-('Hambrosio Comensal','hambrosio.comensal@mkt.bond','56284000','Arroyo Bajo 10 int 58','AO','123456',1000.0);
+('Hambrosio Comensal','hambrosio.comensal@mkt.bond','56284000','Arroyo Bajo 10 int 58','AO','123456',1000.0),
+('Juan Pérez', 'juanp@mkt.bond', '56284099', 'Calle Falsa 123, CDMX, México', 'MX', '1234567890123456789', 1000.0),
+('Maria Gómez', 'mariag@mkt.bond', '56285010', '456 Elm Street, Los Angeles, USA', 'LA', '0987654321098765432', 1000.0),
+('Carlos López', 'carlos@mkt.bond', '56384070', '789 Maple Ave, Toronto, Canada', 'CT', '1112223334445556667', 10000.0);
 
 -- Desconectar
 DISCONNECT;
